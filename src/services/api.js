a8138c5eb93032340962d1d277f5125a9a8f2693
palak <kaurpalakpreet6@gmail.com>
2026-02-17 22:51:32 +0530

    vercel deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL: "https://notes-backend-julo.onrender.com/api"
 });
+
+
 
 export default API;
